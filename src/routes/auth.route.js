8ea86c45d21ba06c5d29180bb993df35aa68ae3e
kirubel12 <kirubelhabte1991@gmail.com>
2025-06-23 17:37:59 +0300

    feat: fix authentication issues

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,15 @@
-import express from "express"
-import { userRegister, userLogin, userLogout } from "../controllers/user.controller.js"
+import express from 'express';
+import { login, logout, refreshToken, register } from '../controllers/auth.controller.js';
+import { verifyRefreshToken } from '../middleware/auth.js';
 
-const authRoute = express.Router()
+const authRoute = express.Router();
 
-authRoute.post("/register",userRegister)
-authRoute.post("/login",userLogin)
-authRoute.post("/logout",userLogout)
+// Public routes
+authRoute.post('/login', login);
+authRoute.post('/register', register);
 
-export default authRoute
+// Protected routes
+authRoute.post('/refresh-token', verifyRefreshToken, refreshToken);
+authRoute.post('/logout', verifyRefreshToken, logout);
+
+export default authRoute;
