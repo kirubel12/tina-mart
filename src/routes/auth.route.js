eb00c813189cf056e1c529407e22eb330ca20859
kirubel12 <kirubelhabte1991@gmail.com>
2025-06-24 12:23:42 +0300

    feat(product): add crud functionality with authenication middleware

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,6 +1,5 @@
 import express from 'express';
-import { login, logout, refreshToken, register } from '../controllers/auth.controller.js';
-import { verifyRefreshToken } from '../middleware/auth.js';
+import { login, register } from '../controllers/auth.controller.js';
 
 const authRoute = express.Router();
 
@@ -9,7 +8,5 @@ authRoute.post('/login', login);
 authRoute.post('/register', register);
 
 // Protected routes
-authRoute.post('/refresh-token', verifyRefreshToken, refreshToken);
-authRoute.post('/logout', verifyRefreshToken, logout);
 
 export default authRoute;
