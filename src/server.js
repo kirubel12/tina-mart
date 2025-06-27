b0be480873d55bf09bfa3f9a7565f6428cbf0427
kirubel12 <kirubelhabte1991@gmail.com>
2025-06-27 11:16:03 +0300

    feat(order): create an order from cart

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express"
 import {ENV} from "./config/env.js"
-import { connectDB } from "./config/db.js"
+import {connectDB} from "./config/db.js"
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import cors from "cors";
@@ -9,10 +9,11 @@ import authRoute from "./routes/auth.route.js";
 import categoryRouter from "./routes/category.route.js";
 import productRoute from "./routes/product.route.js";
 import cartRouter from "./routes/cart.route.js";
+import orderRouter from "./routes/order.route.js";
 
 const app = express()
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(morgan('dev'))
 app.use(helmet())
@@ -21,13 +22,14 @@ app.use(cors({
     credentials: true,
 }))
 
-app.use("/api/v1/auth",authRoute)
-app.use("/api/v1/product",productRoute)
+app.use("/api/v1/auth", authRoute)
+app.use("/api/v1/product", productRoute)
 app.use("/api/v1/category", categoryRouter)
 app.use("/api/v1/cart", cartRouter)
+app.use("/api/v1/order", orderRouter)
 
 
-app.listen(ENV.PORT,  () => {
+app.listen(ENV.PORT, () => {
     connectDB();
     console.log(`Server is running on port ${ENV.PORT}`)
 })
