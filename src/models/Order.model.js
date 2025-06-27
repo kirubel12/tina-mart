91fd674d1b81318ee1c5877af7466ee619c98e5a
kirubel_habte <33315071+kirubel12@users.noreply.github.com>
2025-06-27 12:57:54 +0300

    Feat/order (#6)
    * feat(order): create an order from cart
    
    * feat: add order functionality for order

--- src/models/Order.model.js
+++ src/models/Order.model.js
@@ -1,29 +1,23 @@
 import {Schema, model} from "mongoose";
 
 const orderSchema = new Schema({
-    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    items: [
-      {
-        product: { type: Schema.Types.ObjectId, ref: 'Product' },
-        quantity: Number,
-        price: Number,
-      },
-    ],
-    shippingAddress: { type: String, required: true },
-    paymentMethod: {
-      type: String,
-      enum: ['Cash on Delivery', 'Credit/Debit Card', 'PayPal'],
-      default: 'Cash on Delivery',
+        user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+        cartId: {type: Schema.Types.ObjectId, ref: 'Cart', required: true},
+        shippingAddress: {type: String, required: true},
+        paymentMethod: {
+            type: String,
+            enum: ['Cash on Delivery', 'Credit/Debit Card', 'PayPal'],
+            default: 'Cash on Delivery',
+        },
+        totalPrice: {type: Number, required: true},
+        status: {
+            type: String,
+            enum: ['pending', 'paid', 'shipped', 'delivered', 'cancelled'],
+            default: 'pending',
+        },
+        isPaid: {type: Boolean, default: false},
+        paidAt: Date,
     },
-    totalPrice: { type: Number, required: true },
-    status: {
-      type: String,
-      enum: ['pending', 'paid', 'shipped', 'delivered', 'cancelled'],
-      default: 'pending',
-    },
-    isPaid: { type: Boolean, default: false },
-    paidAt: Date,
-  },
-  { timestamps: true })
+    {timestamps: true})
 
 export const Order = model('Order', orderSchema);
